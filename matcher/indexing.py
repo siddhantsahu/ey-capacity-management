863e36d0d76b5d1a5c5602a45dd9dfb9663fddf5
Siddhant Sahu <siddhant.sahu@foghorn.io>
2020-08-16 21:22:15 +0530

    Added preprocess and updated indexing - starting cleanup

--- matcher/indexing.py
+++ matcher/indexing.py
@@ -7,58 +7,55 @@ from tabulate import tabulate
 
 from skill_match import SkillMatcher
 
-EmployeeSkillPair = namedtuple("ESPair", ["emp_id", "skill_id", "skill_level"])
 
-# TODO: convert to a class?
-def get_emp_skill_pairs(supply_csv, skill_matcher):
-    """Generates employee-skill pairs.
+EmployeeSkillPair = namedtuple("EmployeeSkillPair", ["emp_id", "skill_id"])
+
+
+def get_emp_skill_pairs(emp_skills_csv):
+    """Generates employee-skill pairs. The ingestion part could be easily changed
+    to connect to a database instead of reading from a csv file.
 
     Args:
-        supply_csv: str, path to csv file containing supply data
-        skill_matcher: SkillMatcher instance
+        emp_skills_csv: str, path to file containing skills data for employees
 
     Returns:
-        generator of EmployeeSkillPair
+        generator of tuples
     """
-    df = pd.read_csv(supply_csv, sep="\t").fillna("")
-    df["skill_concat"] = (
-        df[["Sub Unit 1", "Sub Unit 2", "Sub Unit 3", "Skill"]]
-        .agg(" ".join, axis=1)
-        .str.replace("  ", " ")
-    )
-    for i, row in df.iterrows():
-        skill_name = skill_matcher.match(row["skill_concat"])[0]
-        skill_id = skill_matcher.get_idx(skill_name)
-        yield EmployeeSkillPair(row["Name/ID"], skill_id, row["Skill Level"])
+    df = pd.read_csv(emp_skills_csv, sep="\t")
+    for row in df.itertuples():
+        es_pair = EmployeeSkillPair(row.emp_id, row.skill_id)
+        yield es_pair
 
 
 def spimi(es_pairs):
     """Builds the indexing using SPIMI algorithm.
 
     Args:
-        es_pairs: iterable of EmployeeSkillPair
+        es_pairs: iterable of tuples with employee skill pairs
 
     Returns:
         dict, index
     """
     index = {}
     for es in es_pairs:
-        emp_id, skill_id, skill_level = es
+        emp_id, skill_id = es
         if skill_id not in index:
-            index[skill_id] = {emp_id: skill_level}
+            index[skill_id] = [emp_id]
         else:
-            val = index[skill_id]
-            val[emp_id] = skill_level
-            index[skill_id] = val
+            postings_list = index[skill_id]
+            postings_list.append(emp_id)
+            index[skill_id] = postings_list
     return index
 
 
-def main():
-    sm = SkillMatcher("../data/skill_tree.csv")
-    es_pairs = get_emp_skill_pairs("../data/supply.csv", sm)
+def build_index():
+    es_pairs = get_emp_skill_pairs("../data/emp_skills.csv")
     index = spimi(es_pairs)
     return index
 
 
 if __name__ == "__main__":
-    print(main())
+    index = build_index()
+    from pprint import pprint
+
+    pprint(index)
---
+++ matcher/preprocess.py
@@ -0,0 +1,81 @@
+"""Preprocess module.
+
+- Normalizes supply data by breaking it down into skills list and other metadata -
+  experience, location, department etc.
+- Computes maximum values of experience, bench age, rank etc that will be used for
+  standardization in the retrieval module.
+"""
+import pandas as pd
+
+from skill_match import SkillMatcher
+
+
+def normalize_supply():
+    """Break supply.csv into skills and metadata information.
+
+    Usage: python normalize_skills.py
+    """
+    supply = pd.read_csv("../data/supply.csv", sep="\t").fillna("")
+    skills_colnames = [
+        "Name/ID",
+        "Primary Unit",
+        "Sub Unit 1",
+        "Sub Unit 2",
+        "Sub Unit 3",
+        "Skill",
+        "Skill Level",
+    ]
+    meta_colnames = [
+        "Name/ID",
+        "Years of experience",
+        "Rank",
+        "Service Line",
+        "Sub Service Line",
+        "SMU",
+        "Country",
+        "City",
+        "Bench Ageing (weeks)",
+    ]
+
+    skills = (
+        supply[skills_colnames]
+        .rename(columns=lambda x: x.lower().replace(" ", "_"))
+        .rename(columns={"name/id": "emp_id"})
+    )
+    meta = (
+        (supply[meta_colnames].drop_duplicates().reset_index(drop=True))
+        .rename(columns=lambda x: x.lower().replace(" ", "_"))
+        .rename(columns={"name/id": "emp_id", "bench_ageing_(weeks)": "bench_age"})
+    )
+
+    # note: in a proper database, we would't need this - this for the inconsistencies in the
+    # sample data we have, we are mapping the full skill name in supply data to skill tree db
+    skills["cat"] = skills[["sub_unit_1", "sub_unit_2", "sub_unit_3", "skill"]].agg(
+        " ".join, axis=1
+    )
+    sm = SkillMatcher("../data/skill_tree.csv")
+    skills["skill_id"] = skills["cat"].apply(lambda x: sm.get_idx(sm.match(x)[0]))
+
+    # write the information to disk and use these instead of supply.csv
+    meta.to_csv("../data/emp_meta.csv", index=False, sep="\t")
+    skills[["emp_id", "skill_id", "skill_level"]].to_csv(
+        "../data/emp_skills.csv", sep="\t", index=False
+    )
+
+
+def compute_max_values(meta_csv):
+    """Compute maximum values of experience, bench age, rank etc. from meta information.
+
+    In a production scenario, on every insert/update to the employee metadata database,
+    compute this and store it to avoid computing on every request. Essentially, this
+    function should read the cache and return the value - O(1) lookup.
+    """
+    df = pd.read_csv(meta_csv, sep="\t")
+    max_experience = df["experience"].max()
+    max_rank = df["rank"].str.replace("Rank_", "").astype(int).max()
+    max_bench_age = df["bench_age"].max()  # must be in weeks
+    return max_experience, max_rank, max_bench_age
+
+
+if __name__ == "__main__":
+    normalize_supply()
